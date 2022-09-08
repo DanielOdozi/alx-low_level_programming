ebad00a4e74b73e149de837110b234007c1c7d97
Danny111111111 <odozidanielosy@gmail.com>
2022-09-08 10:53:17 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,9 @@
 
 /**
  * main : print the string in the printf function
- * Description - to print with proper grammar, but the outcome is a piece of art,
- * return 0
+ * Description - using the main function
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * return : 0
  */
 int main(void)
 {
