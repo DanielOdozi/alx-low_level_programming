bcd5940f9be4cab6ee760de4fc1190bc507107a8
Danny111111111 <odozidanielosy@gmail.com>
2022-09-08 10:27:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - print the string with the put function
+ *
+ * Description: Using the main function
+ * this program prints: "Programming is like building a multilingual puzzle
+ * return:0
+ */
+
+
 int main(void)
 {
 	char words[] = "\"Programming is like building a multilingual puzzle";
