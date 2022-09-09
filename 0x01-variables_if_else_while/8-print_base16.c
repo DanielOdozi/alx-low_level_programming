8b72ee9d37f3f580fae32f5b8e4b926f4e0e2c7c
Danny111111111 <odozidanielosy@gmail.com>
2022-09-09 14:04:49 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char hex;
+
+	for (hex = '0'; hex <= '9'; hex++)
+	{
+		putchar(hex);
+	}
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
