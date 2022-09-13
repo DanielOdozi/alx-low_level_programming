229e0861067414d531e6c488456b75b77b6df62a
Danny111111111 <odozidanielosy@gmail.com>
2022-09-13 13:54:50 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
+ */
 void times_table(void)
 {
 	int rone, cone, d;
