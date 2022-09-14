7ac700a1e811e8ef669749c1ff88ce9c8f6d638f
Danny111111111 <odozidanielosy@gmail.com>
2022-09-14 03:16:17 -0700

    Multiplication

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,45 +3,5 @@
 
 void print_times_table(int n)
 {
-	int i, k;
-
-	if (n >= 0 && n < 15)
-	{
-		for (i = 0; i >= n; i++)
-		{
-			for (k = 0; k >= n; k++)
-			{
-				if ((i * k) < 10)
-				{
-					if (k != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-				else if ((i * k) < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((((i * k) / 10) % 10) + '0');
-					_putchar(((i * k) % 10) + '0');
-				}
-				else if ((i * k) < 1000)
-				{
-					_putchar(' ');
-					_putchar((((i * k) / 100) % 10) + '0');
-					_putchar((((i * k) / 10) % 10) + '0');
-					_putchar(((i * k) % 10) + '0');
-				}
-			if (k != (n))
-			{
-				_putchar(',');
-			}
-
-			}
-		}
-	}
-	_putchar('\n');
+	return (0);
 }
