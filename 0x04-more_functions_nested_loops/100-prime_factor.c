ce81ecad622581175ab2aa73ec0e5f7ac66980fc
Danny111111111 <odozidanielosy@gmail.com>
2022-09-15 13:07:41 -0700

    Advanced

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * main -  calculate largest prime of 612852475143
+ *
+ * Return: Success Always
+ */
+int main(void)
+{
+	long int i;
+	long int num = 612852475143;
+
+	for (i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+		}
+	}
+	printf("%ld\n", i);
+	return (0);
+}
