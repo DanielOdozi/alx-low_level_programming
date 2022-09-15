80b2d93654a7d60edfa2e5b0ce5bd1c7bc5a5b3b
Danny111111111 <odozidanielosy@gmail.com>
2022-09-15 06:09:56 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_square - Print square line dependent on the integer n.
+ * @n : The number of lines using '#' characters to use per row and column
+ * Return: Void.
+ */
 void print_square(int size)
 {
 	int i;
