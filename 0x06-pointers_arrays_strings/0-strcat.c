59feb545b61e20b583c57e6ec1f9c5d830aaa6ea
Danny111111111 <odozidanielosy@gmail.com>
2022-09-22 01:57:36 -0700

    Pointers and Arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <string.h>
 
-/*
- * @strcat - To append src to dest
- *
- * return: dest
+/**
+ *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
 char *_strcat(char *dest, char *src)
 {
