0aff2032a8362a12f3683f5a7b130f49f59d49d4
Danny111111111 <odozidanielosy@gmail.com>
2022-09-22 02:33:40 -0700

    Pointers and Arrays

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,17 +9,30 @@
 char *rot13(char *c)
 {
 	int i;
+	int j;
 	
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		if ((c[i] >= 'a' && c[i] < 'n') || (c[i] >= 'A' && c[i] < 'N'))
+		if (c[i] >= 'a' && c[i] < 'n')
 		{
 			c[i] += 13;
 		}
-		else if ((c[i] > 'm' && c[i] <= 'z') || (c[i] > 'M' && c[i] <= 'Z'))
+		else if (c[i] > 'm' && c[i] <= 'z')
 		{
 			c[i] -= 13;
 		}
 	}
+	for (j = 0; c[j] != '\0'; j++)
+	{
+		if (c[j] >= 'A' && c[j] < 'N')
+		{
+			c[j] += 13;
+		}
+		else if (c[j] > 'M' && c[j] <= 'Z')
+		{
+			c[j] -= 13;
+		}
+	}
+
 	return (c);
 }
