659f58acbe71223da7577f3c81c72046427cf92d
Danny111111111 <odozidanielosy@gmail.com>
2022-09-30 13:57:21 -0700

    C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,14 +7,12 @@
  * @argv: Array name
  * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	if (argc == 1)
-	{
 		printf("%d\n", argc - 1);
-	}
 	else
 	{
 		for (i = 0; *argv; i++, argv++)
@@ -22,5 +20,6 @@ int main(int argc, char* argv[])
 
 		printf("%d\n", i - 1);
 	}
+
 	return (0);
 }
