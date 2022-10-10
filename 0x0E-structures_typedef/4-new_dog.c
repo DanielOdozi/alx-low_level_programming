d1a67a188f504e6aa78e731633304aa4468e0f1f
Danny111111111 <odozidanielosy@gmail.com>
2022-10-10 05:40:20 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
-	
+
 	if (dog == NULL)
 		return (NULL);
 
 	dog->name = strdup(name);
+
+	if (dog->name == NULL)
+		return (NULL);
+
 	dog->age = age;
+
 	dog->owner = strdup(owner);
+
+	if (dog->owner == NULL)
+		return (NULL);
 
 	return (dog);
 }
