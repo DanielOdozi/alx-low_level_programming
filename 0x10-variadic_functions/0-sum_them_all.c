f6bad07ee05a83dcf63a7620ee8c9359f2e831f0
Danny111111111 <odozidanielosy@gmail.com>
2022-10-13 08:09:00 -0700

    C - Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,23 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * sum_them_all - To sum up all the numbers
- * @ap: The list of all
- * @n: Constant and cannot chaange
- * @sum: sum of all the numbers
- *
- * Return: To the the sum of all the numbers
+ * sum_them_all - Entry point
+ * Desc: Entry
+ *@n: int value
+ * Return: Always 0 (Success)
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum;
+	unsigned int i;
+	int sum;
 
 	if (n == 0)
 		return (0);
-
 	va_start (ap, n);
-
 	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg (ap, int);
-
 	va_end (ap);
 	return (sum);
 }
