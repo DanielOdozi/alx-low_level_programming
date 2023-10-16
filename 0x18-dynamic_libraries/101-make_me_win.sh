84a8666ab73e50e36afec91caabbc1f75c3d0a62
root <root@DanielOdozi>
2023-10-16 23:52:07 +0100

    DLL

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/l01-make_me_win.so https://github.com/DanielOdozi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/l01-make_me_win.so
-export LD_PRELOAD=$HOME/l01-make_me_win.so
+wget -O /tmp/win.so https://github.com/DanielOdozi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
