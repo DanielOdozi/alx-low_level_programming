03114ec60866567b3404aa6809ebc8bd668e059f
root <root@DanielOdozi>
2023-10-16 22:22:17 +0100

    last

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DYNLIB
+#define DYNLIB
 
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*MAIN_H*/
+#endif
