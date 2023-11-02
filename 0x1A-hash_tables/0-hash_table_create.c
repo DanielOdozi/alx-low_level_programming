9139c4edfc40037126989bd10ad0643d05cc66d9
root <root@DanielOdozi>
2023-11-02 15:45:48 +0100

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Create a hash table
+ * @size: The size of the table
+ *
+ * Return: size of hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new_table = NULL;
